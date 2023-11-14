235d3e965e406575884d6e83d8d17213d97e8b05
FuryACE007 <sudhanshuvshekhar@gmail.com>
2023-11-14 16:53:32 +0530

    added polyfils

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { Box, Button, TextField, Typography } from "@mui/material";
 
@@ -10,6 +11,10 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
 
 function App() {
   const tokenNameRef = useRef<HTMLInputElement | null>(null);
@@ -24,60 +29,65 @@ function App() {
     "https://solana-devnet.g.alchemy.com/v2/" +
     process.env.REACT_APP_RPC_ENDPOINT;
 
-  const wallet = new PhantomWalletAdapter();
+  const solNetwork = WalletAdapterNetwork.Devnet;
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [solNetwork]);
 
   return (
     <ConnectionProvider endpoint={rpcEndpoint}>
-      <WalletProvider wallets={[wallet]}>
-        <Box
-          sx={{
-            width: "100%",
-            height: "100vh",
-            bgcolor: "#BEADFA",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <WalletMultiButton />
           <Box
-            component="form"
             sx={{
+              width: "100%",
+              height: "100vh",
+              bgcolor: "#BEADFA",
               display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            <Typography variant="h5" color="secondary">
-              Token Minting Machine
-            </Typography>
-            <TextField
-              id="token-name"
-              label="Token Name"
-              variant="outlined"
-              placeholder="Enter the Token name"
-              ref={tokenNameRef}
-            />
-            <TextField
-              id="token-symbol"
-              label="Token Symbol"
-              variant="outlined"
-              placeholder="$TOKEN"
-              ref={tokenSymbolRef}
-            />
-            <TextField
-              id="token-description"
-              label="Token Description"
-              multiline
-              rows={4}
-              variant="outlined"
-              placeholder="Enter the Token description"
-              ref={tokenDescriptionRef}
-            />
-            <Button variant="contained" color="secondary">
-              Mint Token
-            </Button>
+            <Box
+              component="form"
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "1rem",
+              }}
+            >
+              <Typography variant="h5" color="secondary">
+                Token Minting Machine
+              </Typography>
+
+              <TextField
+                id="token-name"
+                label="Token Name"
+                variant="outlined"
+                placeholder="Enter the Token name"
+                ref={tokenNameRef}
+              />
+              <TextField
+                id="token-symbol"
+                label="Token Symbol"
+                variant="outlined"
+                placeholder="$TOKEN"
+                ref={tokenSymbolRef}
+              />
+              <TextField
+                id="token-description"
+                label="Token Description"
+                multiline
+                rows={4}
+                variant="outlined"
+                placeholder="Enter the Token description"
+                ref={tokenDescriptionRef}
+              />
+              <Button variant="contained" color="secondary">
+                Mint Token
+              </Button>
+            </Box>
           </Box>
-        </Box>
+        </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   );
